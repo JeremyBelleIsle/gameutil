fe33081ab223f05c279e42a7055239560bca5d2e
jeremybelleisle <jeremy.belleisle@gmail.com>
2026-02-03 16:55:30 -0500

    ballPhysics

--- ballPhysics.go
+++ ballPhysics.go
@@ -6,13 +6,13 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-type platform struct {
+type Platform struct {
 	x, y float64
 	w, h float64
 	clr  color.RGBA
 }
 
-func HorizontalCollisions(playerX, playerY, playerW, playerH float64, platforms []platform) {
+func HorizontalCollisions(playerX, playerY, playerW, playerH float64, platforms []Platform) {
 	for _, plat := range platforms {
 		if RectColl(playerX, playerY, playerW, playerH, plat.x, plat.y, plat.w, plat.h) {
 			if ebiten.IsKeyPressed(ebiten.KeyLeft) {
