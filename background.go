486b50b7c38bb4fe290815d5ec4208c45bd54dc5
jeremybelleisle <jeremy.belleisle@gmail.com>
2026-04-05 20:31:47 -0400

    CPU reduction - background

--- background.go
+++ background.go
@@ -7,53 +7,62 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
-func GenerateBackgroundWithDegraded(screen *ebiten.Image, principalColor color.RGBA64, screenWidth, screenHeight float64, VOrH string) {
+var (
+	bgRectImage   *ebiten.Image
+	bgCircleImage *ebiten.Image
+)
 
-	currentColor := color.RGBA64{principalColor.R / 2, principalColor.G / 2, principalColor.B / 2, principalColor.A / 2}
+func InitBackgrounds(principalColor color.RGBA64, screenWidth, screenHeight int) {
+	bgRectImage = ebiten.NewImage(screenWidth, screenHeight)
+	drawRectDegraded(bgRectImage, principalColor, float64(screenWidth), float64(screenHeight))
 
-	switch VOrH {
-	case "h":
-		for i := 0; i < int(screenWidth); i++ {
-			vector.DrawFilledRect(screen, float32(i), 0, 1, float32(screenHeight), currentColor, false)
-			r := uint16((principalColor.R / 2) / uint16(screenWidth))
-			g := uint16((principalColor.G / 2) / uint16(screenWidth))
-			b := uint16((principalColor.B / 2) / uint16(screenWidth))
-			a := uint16((principalColor.A / 2) / uint16(screenWidth))
+	bgCircleImage = ebiten.NewImage(screenWidth, screenHeight)
+	drawCircleDegraded(bgCircleImage, principalColor, float64(screenWidth), float64(screenHeight))
+}
 
-			currentColor = color.RGBA64{R: currentColor.R + r, G: currentColor.G + g, B: currentColor.B + b, A: currentColor.A + a}
-		}
-	case "v":
-		for i := 0; i < int(screenHeight); i++ {
-			vector.DrawFilledRect(screen, 0, float32(i), float32(screenWidth), 1, currentColor, false)
-			r := uint16((principalColor.R / 2) / uint16(screenHeight))
-			g := uint16((principalColor.G / 2) / uint16(screenHeight))
-			b := uint16((principalColor.B / 2) / uint16(screenHeight))
-			a := uint16((principalColor.A / 2) / uint16(screenHeight))
-
-			currentColor = color.RGBA64{R: currentColor.R + r, G: currentColor.G + g, B: currentColor.B + b, A: currentColor.A + a}
-		}
+func DrawBackground(screen *ebiten.Image) {
+	if bgRectImage != nil {
+		screen.DrawImage(bgRectImage, nil)
 	}
 }
 
-func GenerateBackgroundWithDegradedCircle(screen *ebiten.Image, principalColor color.RGBA64, screenWidth, screenHeight float64) {
-	maxRadius := screenHeight
-	if screenWidth < screenHeight {
-		maxRadius = screenWidth
+func DrawCircleBackground(screen *ebiten.Image) {
+	if bgCircleImage != nil {
+		screen.DrawImage(bgCircleImage, nil)
+	}
+}
+
+func drawRectDegraded(dest *ebiten.Image, principalColor color.RGBA64, w, h float64) {
+	currentColor := color.RGBA64{principalColor.R / 2, principalColor.G / 2, principalColor.B / 2, principalColor.A / 2}
+
+	r := uint16((principalColor.R / 2) / uint16(w))
+	g := uint16((principalColor.G / 2) / uint16(w))
+	b := uint16((principalColor.B / 2) / uint16(w))
+	a := uint16((principalColor.A / 2) / uint16(w))
+
+	for i := 0; i < int(w); i++ {
+		vector.DrawFilledRect(dest, float32(i), 0, 1, float32(h), currentColor, false)
+		currentColor.R += r
+		currentColor.G += g
+		currentColor.B += b
+		currentColor.A += a
+	}
+}
+
+func drawCircleDegraded(dest *ebiten.Image, principalColor color.RGBA64, w, h float64) {
+	maxRadius := h
+	if w < h {
+		maxRadius = w
 	}
 
-	// Dessiner du plus grand cercle au plus petit pour voir le dégradé
 	for i := int(maxRadius); i >= 0; i-- {
-		// Calculer un ratio de progression (0 à 1)
 		ratio := float64(i) / maxRadius
-
-		// Créer un dégradé de couleur en partant de la couleur principale
 		currentColor := color.RGBA64{
 			R: uint16(float64(principalColor.R) * ratio),
 			G: uint16(float64(principalColor.G) * ratio),
 			B: uint16(float64(principalColor.B) * ratio),
 			A: principalColor.A,
 		}
-
-		vector.DrawFilledCircle(screen, float32(screenWidth)/2, float32(screenHeight)/2, float32(i), currentColor, false)
+		vector.DrawFilledCircle(dest, float32(w)/2, float32(h)/2, float32(i), currentColor, false)
 	}
 }
